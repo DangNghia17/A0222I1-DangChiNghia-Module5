c45e5af5cb2aa784645005dd174c10cd595f4041
DangNghia17 <dnghia2002@gmail.com>
2022-12-12 21:26:02 +0700

    21g25 12/12

--- ss5-components-interaction/bai-tap-thuc-hanh/src/app/register/register.component.ts
+++ ss5-components-interaction/bai-tap-thuc-hanh/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -12,12 +12,29 @@ export class RegisterComponent implements OnInit {
 
   constructor() {
     this.register = new FormGroup({
-email : new FormControl("",[Validators.required])
-    }
-  })
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl("", [Validators.required]),
+      country: new FormControl("", [Validators.required]),
+      age: new FormControl("", [Validators.required, Validators.min(18)]),
+      gender: new FormControl("", [Validators.required]),
+      phone: new FormControl("", [Validators.required, Validators.pattern("(84)+([0-9]{8})")])
+    });
+  }
+
+  // validateConfirmPassword(control :AbstractControl){
+  //   let password = control.value;
+  //   if (password == )
+  // }
 
 
   ngOnInit(): void {
   }
 
+  createAccount() {
+    console.log(this.register);
+    if (this.register.valid){
+      this.createAccount;
+    }
+  }
 }
